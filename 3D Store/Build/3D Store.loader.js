d72de6246177667f60e1a196f013579da834d4a7
Bristidev Burman <bristidev2004@gmail.com>
2025-04-18 00:10:28 +0530

    Delete 3D Store directory

--- 3D Store/Build/3D Store.loader.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b85044158890046296ce72adce762bddd5648c914c48a4ba2e99e412d2899055
-size 47405
--- 3D Store/Build/WebGL Build.loader.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:f155438b2c535b4e64913e7b16c5a7aa46ed1e42ed6ff82f998ad82c7099b3fd
-size 47405
